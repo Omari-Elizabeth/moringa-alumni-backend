12f3d5d3aa22f995cb644c4927880cd331220d97
James Njenga <knjenga94@gmail.com>
2023-01-09 21:39:28 +0300

    MC-18 <Write user model validation tests>

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_09_090332) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_09_120023) do
   create_table "users", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "username"
     t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
