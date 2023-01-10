32f47e6b0f7e225d72f474f4302b35d2421d54fd
James Njenga <knjenga94@gmail.com>
2023-01-10 14:34:11 +0300

    MC-42 <Write tests for profile model and implemented validations>

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,11 @@
 class Profile < ApplicationRecord
-    belongs_to :user
+    belongs_to :user 
+
+    # validates :my_field, format: { with: /.+sd.+/ }
+
+    validates :cohort,format: { with: /.*(sd|ds).*/i }
+    # validates :cohort,inclusion:{in:["sd","ds"]}
+
+    validates :gender,inclusion:{in:["male","female"]}
+    
 end
