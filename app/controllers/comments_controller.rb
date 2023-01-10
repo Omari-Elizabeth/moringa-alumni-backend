59ebc8f2b12d90b22179f1774cc3785256a4bf00
puritymwaura <purity.mwaura@student.moringaschool.com>
2023-01-10 08:34:45 +0300

    MC-39 <done>

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -46,6 +46,6 @@ class CommentsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def comment_params
-      params.require(:comment).permit(:comment_msg, :user_id, :post_id)
+      params.permit(:comment_msg, :user_id, :post_id)
     end
 end
