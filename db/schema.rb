a2ce0d63c651d98f2e427b1925a2c8730c529b14
JohnstoneDev <marucha.webdev@yahoo.com>
2023-01-10 23:16:52 +0300

    Patch for seeding Error, add password_confirmation to admin params

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_09_084832) do
-
-
+ActiveRecord::Schema[7.0].define(version: 2023_01_10_051626) do
   create_table "admins", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
@@ -27,15 +25,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_09_084832) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["admin_id"], name: "index_announcements_on_admin_id"
-    end
-    
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "password_digest"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
-
 
   create_table "comments", force: :cascade do |t|
     t.string "comment_msg"
@@ -52,7 +42,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_09_084832) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
 
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
