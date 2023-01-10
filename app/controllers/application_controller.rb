328849cbcc555a81dad8b276e7d7293b1132207c
Omari-Elizabeth <elizabeth.omari@student.moringaschool.com>
2023-01-10 10:33:08 +0300

    add seed file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::API
-    
-    skip_before_action :verify_authenticity_token
+
+    # skip_before_action :verify_authenticity_token
 
     include ActionController::Cookies
 end
