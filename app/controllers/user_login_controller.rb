3723250a59a081cdba6c7f146926747e4fe278c9
James Njenga <knjenga94@gmail.com>
2023-01-11 11:31:42 +0300

    MC-21 <Implement create action on user_login_controller>

--- app/controllers/user_login_controller.rb
+++ app/controllers/user_login_controller.rb
@@ -1,2 +1,19 @@
 class UserLoginController < ApplicationController
+
+    # POST/login-user 
+    def create
+        user=User.find_by(username:user_params[:username])
+        if user&.authenticate(params[:password])
+            token=encode_token(user_id:user.id)
+            render json:{user:user,token:token}, status: :ok
+        else
+         render json: {error:"Invalid username or password"}, status: :unprocessable_entity
+        end
+    end
+
+    private
+    def user_params
+        params.permit(:username,:password,:password_confirmation)
+    end
+
 end
