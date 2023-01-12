f9c96a58a46ae533bbcf406c3ecb6f70bb0cf548
Omari-Elizabeth <elizabeth.omari@student.moringaschool.com>
2023-01-12 15:41:09 +0300

    fix migration bugs

--- db/schema.rb
+++ db/schema.rb
@@ -58,7 +58,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_11_122103) do
     t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
-
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
@@ -66,4 +65,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_11_122103) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "profiles", "users"
 end
