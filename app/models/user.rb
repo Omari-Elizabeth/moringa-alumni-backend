4fda2a83e0be48a62e255363afd207a2068528ac
James Njenga <knjenga94@gmail.com>
2023-01-19 22:18:59 +0300

    Remove password validation and add error handling to users controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
     validates :username,presence:true,uniqueness:true,length:{minimum:3}
-    validates :password_digest,length:{minimum:4}
+    # validates :password_digest,length:{minimum:4}
     has_one :profile
 end
