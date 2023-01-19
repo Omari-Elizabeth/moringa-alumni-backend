fcdd9bfc44634d192565b69e8632af9f53d5f732
James Njenga <knjenga94@gmail.com>
2023-01-19 23:12:53 +0300

    Add image attachment on profile model and serializer

--- app/serializers/profile_serializer.rb
+++ app/serializers/profile_serializer.rb
@@ -1,4 +1,9 @@
 class ProfileSerializer < ActiveModel::Serializer
-  attributes :id, :fname, :lname, :gender, :cohort, :birthday, :profession, :avatar
+  attributes :id, :fname, :lname, :gender, :cohort, :birthday, :profession, :avatar_url
   belongs_to :user
+
+  def avatar_url
+    object.avatar.url
+  end
+  
 end
